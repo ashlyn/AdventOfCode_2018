43c243afbd7aabc900f40e86220e45c7d2c22f91
Ashlyn Lee <ashlynlee13@gmail.com>
2018-12-02 18:20:44 -0600

    Day 2 Part 2

--- Solvers/Day2Solver.cs
+++ Solvers/Day2Solver.cs
@@ -17,7 +17,17 @@ public class Day2Solver : ISolver
 
   public string SolvePart2()
   {
-    throw new System.NotImplementedException();
+    var idLines = File.ReadAllLines("Inputs/Day2.txt");
+
+    foreach (string id1 in idLines)
+    {
+      foreach (string id2 in idLines)
+      {
+        var difference = GetDifference(id1, id2);
+        if (difference == 1) return GetSharedCharacters(id1, id2);
+      }
+    }
+    throw new InvalidOperationException("No IDs match all but one character.");
   }
 
   private Dictionary<char, int> GetCharCountsFromId(string id)
@@ -46,5 +56,29 @@ public class Day2Solver : ISolver
   private bool HasExactlyTwoCharacterExactlyTwice(Dictionary<char, int> map)
   {
     return map.ContainsValue(2);
+  }
+
+  private int GetDifference(string id1, string id2)
+  {
+    var difference = 0;
+    for (int i = 0; i < id1.Length; i++)
+    {
+      difference += id1[i] == id2[i] ? 0 : 1;
+    }
+    return difference;
+  }
+
+  private string GetSharedCharacters(string id1, string id2)
+  {
+    var id1Array = id1.ToCharArray();
+    var id2Array = id2.ToCharArray();
+    for (int i = 0; i < id1Array.Length; i++)
+    {
+      if (id1Array[i] != id2Array[i])
+      {
+        id1Array[i] = '\0';
+      }
+    }
+    return new String(id1Array);
   }
 }
