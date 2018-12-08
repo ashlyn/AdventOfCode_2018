6a47e1d454778d1642fd4fff25cb3d59a6e3345a
Ashlyn Lee <ashlynlee13@gmail.com>
2018-12-08 13:28:02 -0600

    Day 8 Part 2

--- Solvers/Day8Solver.cs
+++ Solvers/Day8Solver.cs
@@ -5,41 +5,93 @@ using System.Linq;
 
 public class Day8Solver : ISolver
 {
-  public string SolvePart1()
+  internal class Node
   {
-    var input = File.ReadAllText("Inputs/Day8.txt")
-      .Replace(Environment.NewLine, "")
-      .Split(" ")
-      .Select(i => int.Parse(i));
+    public List<int> Metatdata { get; set; } = new List<int>();
+    public List<Node> Children { get; set; } = new List<Node>();
 
-    var index = 0;
-    return BuildTree(input, 0, ref index).ToString();
+    public int Value()
+    {
+      if (Children.Count() == 0)
+      {
+        return Sum();
+      }
 
-    throw new System.NotImplementedException();
+      var value = 0;
+      foreach (var meta in Metatdata)
+      {
+        if (meta <= Children.Count())
+        {
+          value += Children.ElementAt(meta - 1).Value();
+        }
+      }
+
+      return value;
+    }
+
+    public int Sum()
+    {
+      return Metatdata.Sum(m => m);
+    }
+
+    public int SumChildren()
+    {
+      return Sum() + Children.Sum(c => c.SumChildren());
+    }
   }
 
-  private int BuildTree(IEnumerable<int> input, int metadataSum, ref int i)
+  private Node RootNode;
+
+  public string SolvePart1()
+  {
+    if (RootNode == null)
+    {
+      var input = File.ReadAllText("Inputs/Day8.txt")
+      .Replace(Environment.NewLine, "")
+        .Split(" ")
+        .Select(i => int.Parse(i));
+
+      var index = 0;
+      RootNode = BuildTree(input, ref index);
+    }
+
+    return RootNode.SumChildren().ToString();
+  }
+
+  public string SolvePart2()
+  {
+    if (RootNode == null)
+    {
+      var input = File.ReadAllText("Inputs/Day8.txt")
+      .Replace(Environment.NewLine, "")
+        .Split(" ")
+        .Select(i => int.Parse(i));
+
+      var index = 0;
+      RootNode = BuildTree(input, ref index);
+    }
+
+    return RootNode.Value().ToString();
+  }
+
+  private Node BuildTree(IEnumerable<int> input, ref int i)
   {
     int childNodesRemaining = input.ElementAt(i++);
     int metadataEntries = input.ElementAt(i++);
+    var node = new Node();
 
     while (childNodesRemaining > 0)
     {
-      metadataSum = BuildTree(input, metadataSum, ref i);
+      node.Children.Add(BuildTree(input, ref i));
       childNodesRemaining--;
     }
 
     while (metadataEntries > 0)
     {
-      metadataSum += input.ElementAt(i++);
+      node.Metatdata.Add(input.ElementAt(i++));
       metadataEntries--;
     }
 
-    return metadataSum;
-  }
-
-  public string SolvePart2()
-  {
-    throw new System.NotImplementedException();
+    return node;
   }
 }
