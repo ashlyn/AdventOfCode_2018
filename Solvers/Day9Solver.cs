6d2c1dbc35ff877c7f101d68172901f16d56b5ba
Ashlyn Lee <ashlynlee13@gmail.com>
2018-12-09 18:56:22 -0600

    Refactor to use LinkedList and double so ints don't overflow and Part 2 runs in a reasonable time

--- Solvers/Day9Solver.cs
+++ Solvers/Day9Solver.cs
@@ -7,50 +7,42 @@ public class Day9Solver : ISolver
   // input string: "470 players; last marble is worth 72170 points"
   private readonly int _playerCount = 470;
   private int _marbleCount = 72170;
+  private const int TakeMarbleRelativePosition = 7;
 
   public string SolvePart1()
   {
-    var circle = new List<int> { 0 };
-    var players = new int[_playerCount];
-    var currentMarblePosition = 0;
+    var circle = new LinkedList<int>();
+    var currentMarbleNode = circle.AddFirst(0);
+    var players = new double[_playerCount];
     var marblesPlayed = 1;
 
     var currentPlayer = 0;
-    try
+    while (marblesPlayed < _marbleCount)
     {
-
-      while (marblesPlayed < _marbleCount)
+      if (MarbleScoring(marblesPlayed))
       {
-        if (MarbleScoring(marblesPlayed))
+        // add to the current player's score
+        players[currentPlayer] += marblesPlayed;
+        // find the marble to take
+        for (int i = 0; i < TakeMarbleRelativePosition; i++)
         {
-          // add to the current player's score
-          players[currentPlayer] += marblesPlayed;
-          // find the marble to take
-          var marblePositionToTake = MarbleToTake(currentMarblePosition, circle.Count);
-          players[currentPlayer] += circle[marblePositionToTake];
-          circle.RemoveAt(marblePositionToTake);
-          // find the new current marble
-          currentMarblePosition = marblePositionToTake;
+          currentMarbleNode = currentMarbleNode.Previous ?? circle.Last;
         }
-        else
-        {
-          // put the marble in the circle
-          var newPosition = NewMarblePosition(currentMarblePosition, circle.Count);
-          circle.Insert(newPosition, marblesPlayed);
-          currentMarblePosition = newPosition;
-        }
-        marblesPlayed++;
-        currentPlayer = (currentPlayer + 1) % _playerCount;
+        var marbleToTake = currentMarbleNode;
+        players[currentPlayer] += marbleToTake.Value;
+        currentMarbleNode = marbleToTake.Next;
+        circle.Remove(marbleToTake);
       }
+      else
+      {
+        // put the marble in the circle
+        currentMarbleNode = circle.AddAfter(currentMarbleNode.Next ?? circle.First, marblesPlayed);
+      }
+      marblesPlayed++;
+      currentPlayer = (currentPlayer + 1) % _playerCount;
+    }
 
-      return players.Max().ToString();
-    }
-    catch (Exception e)
-    {
-      Console.WriteLine(e.StackTrace);
-      Console.WriteLine(e.InnerException.StackTrace);
-      throw;
-    }
+    return players.Max().ToString();
   }
 
   public string SolvePart2()
@@ -62,15 +54,5 @@ public class Day9Solver : ISolver
   private bool MarbleScoring(int marbleNumber)
   {
     return (marbleNumber % 23) == 0;
-  }
-
-  private int MarbleToTake(int i, int countMarblesInCircle)
-  {
-    return (((i - 7) % countMarblesInCircle) + countMarblesInCircle) % countMarblesInCircle;
-  }
-
-  private int NewMarblePosition(int i, int countMarblesInCircle)
-  {
-    return (i + 2) % countMarblesInCircle;
   }
 }
